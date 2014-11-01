c4ca4cef05ab576677f87b492a5e257c0d874bd3
Max <1@porzelt.net>
2014-11-01 18:07:34 +0100

    small Database preperation

--- PurpleDatabase.cs
+++ PurpleDatabase.cs
@@ -11,177 +11,174 @@ using System.Collections.Generic;
 // TODO: Work in progress
 // TODO: make it mac compatible
 
-public class PurpleDatabase : MonoBehaviour {
+namespace PurpleDatabase
+{
+	public class PurpleDatabase : MonoBehaviour {
 
-	private string serverIP = PurpleConfig.Database.IP;
-	private string serverDatabase = PurpleConfig.Database.Name;
-	private string serverUser = PurpleConfig.Database.User;
-	private string serverPassword = PurpleConfig.Database.Password;
+		private string serverIP = PurpleConfig.Database.IP;
+		private string serverDatabase = PurpleConfig.Database.Name;
+		private string serverUser = PurpleConfig.Database.User;
+		private string serverPassword = PurpleConfig.Database.Password;
 
-	private string connectionString;
-	private MySqlConnection connection;
+		private string connectionString;
+		private MySqlConnection connection;
 
-	void Start () {
-		Initialize ();
-		SelectStatement();
-	}
-
-
-
-
-
-	// move to network manager
-	private bool checkLogin(string username, string password){
-
-		string pwDatabaseHash = "dummy";
-
-		PurplePassword pm = new PurplePassword ();
-		bool pwValid = pm.ValidatePassword (password, pwDatabaseHash);
-		return pwValid;
-	}
+		protected PurpleDatabase () {
+			Initialize ();
+			SelectStatement();
+		}
 
 
+		// move to network manager
+		private bool checkLogin(string username, string password){
+
+			string pwDatabaseHash = "dummy";
+
+			PurplePassword pm = new PurplePassword ();
+			bool pwValid = pm.ValidatePassword (password, pwDatabaseHash);
+			return pwValid;
+		}
 
 
-
-	// TODO - make all statements dynamic
-	//Select statement
-	public List< string >[] SelectStatement()
-	{
-		string query = "SELECT * FROM `one`"; // select statement
-
-		//Create a list to store the result
-		List< string >[] list = new List< string >[3];
-		list[0] = new List< string >();
-		list[1] = new List< string >();
-		list[2] = new List< string >();
-
-		if (OpenConnection() == true)
+		// TODO - make all statements dynamic
+		//Select statement
+		public List< string >[] SelectStatement()
 		{
-			MySqlCommand cmd = new MySqlCommand(query, connection);
-			MySqlDataReader dataReader = cmd.ExecuteReader();
+			string query = "SELECT * FROM `one`"; // select statement
 
-			//Read the data and store them in the list
-			while (dataReader.Read())
+			//Create a list to store the result
+			List< string >[] list = new List< string >[3];
+			list[0] = new List< string >();
+			list[1] = new List< string >();
+			list[2] = new List< string >();
+
+			if (OpenConnection() == true)
 			{
-				list[0].Add(dataReader["id"] + "");
-				list[1].Add(dataReader["name"] + "");
-				list[2].Add(dataReader["counter"] + "");
-				Debug.Log (dataReader["id"] + " " + dataReader["name"] + " " + dataReader["counter"]);
+				MySqlCommand cmd = new MySqlCommand(query, connection);
+				MySqlDataReader dataReader = cmd.ExecuteReader();
+
+				//Read the data and store them in the list
+				while (dataReader.Read())
+				{
+					list[0].Add(dataReader["id"] + "");
+					list[1].Add(dataReader["name"] + "");
+					list[2].Add(dataReader["counter"] + "");
+					Debug.Log (dataReader["id"] + " " + dataReader["name"] + " " + dataReader["counter"]);
+				}
+
+				dataReader.Close();
+				CloseConnection();
+
+				return list;
 			}
-
-			dataReader.Close();
-			CloseConnection();
-
-			return list;
+			else
+			{
+				return list;
+			}
 		}
-		else
+
+		// Insert statement
+		private void InsertStatement()
 		{
-			return list;
+			string query = ""; // Insert query
+
+			if (OpenConnection () == true) {
+				MySqlCommand cmd = new MySqlCommand (query, connection);
+
+				cmd.ExecuteNonQuery ();
+				CloseConnection ();
+			}
 		}
-	}
 
-	// Insert statement
-	private void InsertStatement()
-	{
-		string query = ""; // Insert query
-
-		if (OpenConnection () == true) {
-			MySqlCommand cmd = new MySqlCommand (query, connection);
-
-			cmd.ExecuteNonQuery ();
-			CloseConnection ();
-		}
-	}
-
-	//Update statement
-	private void UpdateStatement()
-	{
-		string query = ""; // Update query
-
-		if (OpenConnection() == true)
+		//Update statement
+		private void UpdateStatement()
 		{
-			MySqlCommand cmd = new MySqlCommand();
-			cmd.CommandText = query;
-			cmd.Connection = connection;
+			string query = ""; // Update query
 
-			cmd.ExecuteNonQuery();
-			CloseConnection();
+			if (OpenConnection() == true)
+			{
+				MySqlCommand cmd = new MySqlCommand();
+				cmd.CommandText = query;
+				cmd.Connection = connection;
+
+				cmd.ExecuteNonQuery();
+				CloseConnection();
+			}
 		}
-	}
 
-	//Delete statement
-	private void DeleteStatement()
-	{
-		string query = ""; // Delete query
-
-		if (OpenConnection() == true)
+		//Delete statement
+		private void DeleteStatement()
 		{
-			MySqlCommand cmd = new MySqlCommand(query, connection);
+			string query = ""; // Delete query
 
-			cmd.ExecuteNonQuery();
-			CloseConnection();
+			if (OpenConnection() == true)
+			{
+				MySqlCommand cmd = new MySqlCommand(query, connection);
+
+				cmd.ExecuteNonQuery();
+				CloseConnection();
+			}
 		}
-	}
 
-	/************************************/
+		/************************************/
 
-	// initialize database
-	private void Initialize(){
-		if(connection == null || connection.State != ConnectionState.Open){
+		// initialize database
+		private void Initialize(){
+			if(connection == null || connection.State != ConnectionState.Open){
+				try
+				{
+					connectionString = "Server="+serverIP+";" +
+						"Database="+serverDatabase+";" +
+						"User ID="+serverUser+";" +
+						"Pooling=false;" +
+						"Password="+serverPassword+";";
+					connection = new MySqlConnection (connectionString);
+				}
+				catch (Exception ex)
+				{
+					Debug.Log(ex.ToString());
+				}
+			}
+		}
+
+		private bool OpenConnection()
+		{
 			try
 			{
-				connectionString = "Server="+serverIP+";" +
-					"Database="+serverDatabase+";" +
-					"User ID="+serverUser+";" +
-					"Pooling=false;" +
-					"Password="+serverPassword+";";
-				connection = new MySqlConnection (connectionString);
+				connection.Open();
+				return true;
 			}
-			catch (Exception ex)
+			catch (MySqlException ex)
 			{
-				Debug.Log(ex.ToString());
+				//0: Cannot connect to server.
+				//1045: Invalid user name and/or password.
+				switch (ex.Number)
+				{
+				case 0:
+					Debug.Log("Cannot connect to server.  Contact administrator");
+					break;
+
+				case 1045:
+					Debug.Log("Invalid username/password, please try again");
+					break;
+				}
+				return false;
 			}
 		}
-	}
 
-	private bool OpenConnection()
-	{
-		try
+		//Close connection
+		private bool CloseConnection()
 		{
-			connection.Open();
-			return true;
-		}
-		catch (MySqlException ex)
-		{
-			//0: Cannot connect to server.
-			//1045: Invalid user name and/or password.
-			switch (ex.Number)
+			try
 			{
-			case 0:
-				Debug.Log("Cannot connect to server.  Contact administrator");
-				break;
-
-			case 1045:
-				Debug.Log("Invalid username/password, please try again");
-				break;
+				connection.Close();
+				return true;
 			}
-			return false;
-		}
-	}
-
-	//Close connection
-	private bool CloseConnection()
-	{
-		try
-		{
-			connection.Close();
-			return true;
-		}
-		catch (MySqlException ex)
-		{
-			Debug.Log(ex.Message);
-			return false;
+			catch (MySqlException ex)
+			{
+				Debug.Log(ex.Message);
+				return false;
+			}
 		}
 	}
 }
