5ca22058af1faaf1b6753f0d3a64ed23ba5d0417
Max <1@porzelt.net>
2014-11-03 19:58:12 +0100

    Purple Storrage
    Own storage class…

---
+++ PurpleStorage.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+// TODO: Data storage for Client data
+// PlayerPrefs
+// as well as File saving
+
+// perhaps own object for storage
+
+namespace PurpleNetwork
+{
+	public class PurpleStorage : MonoBehaviour
+	{
+
+		// START UP /////////////////////////
+		protected PurpleStorage ()
+		{
+			//
+		}
+
+		public void dummy()
+		{
+			//
+		}
+	}
+}
