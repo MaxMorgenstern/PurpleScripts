1c9c3a1b28f212aa510cab0043a8370fa83c45fc
Max <1@porzelt.net>
2014-11-13 23:56:42 +0100

    purple hash added

---
+++ PurpleHash.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+// TODO: class for hashing
+// something like http://support.microsoft.com/kb/307020/de
+
+// TODO: mov into helper
+
+public class PurpleHash : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
+
