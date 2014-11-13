3707deb1eb119bb1994aa14cc0a67db3d9767fbc
Max <1@porzelt.net>
2014-11-13 22:26:27 +0100

    Delegate fix

--- Helper/PurpleNetworkDelegates.cs
+++ Helper/PurpleNetworkDelegates.cs
@@ -2,6 +2,6 @@ using UnityEngine;
 namespace PurpleNetwork
 {
 	// DELEGATES FOR CALLBACK AND EVENTS
-	public delegate void PurpleNetCallback(object converted_object, NetworkPlayer network_info); // With message
+	public delegate void PurpleNetCallback(string converted_object, NetworkPlayer network_info); // With message
 	public delegate void PurpleNetworkEvent(object passed_object, NetworkPlayer network_info); // network event
 }
