ebf8ccb2a982450e0d863f90bb6dc54cc712daa1
Max <1@porzelt.net>
2014-11-14 21:44:29 +0100

    hash extended

--- PurpleHash.cs
+++ PurpleHash.cs
@@ -5,17 +5,39 @@ using System.Collections;
 // something like http://support.microsoft.com/kb/307020/de
 
 // TODO: mov into helper
+using System.Security.Cryptography;
+using System.Text;
 
 public class PurpleHash
 {
-	public static int CalculateHash (object hashObject)
+	public static string CalculateHash (object hashObject)
 	{
-		return CalculateHash(PurpleSerializer.ObjectToStringConverter (hashObject));
+		return CalculateHash(PurpleSerializer.ObjectToStringConverter (hashObject), hashObject.ToString());
 	}
 	
-	public static int CalculateHash (string hashObject)
+	public static string CalculateHash (string hashObject, string hashObjectType)
 	{
-		return hashObject.GetHashCode ();
+		return convert_string_to_MD5(hashObject.ToString() + hashObjectType.ToString());
+	}
+
+	public static string CalculateHash (string hashObject)
+	{
+		return convert_string_to_MD5(hashObject.ToString());
+	}
+
+
+
+	private const string _myGUID = "{C05ACA39-C810-4DD1-B138-41603713DD8A}";
+	private static string convert_string_to_MD5(string strword)
+	{
+		MD5 md5 = MD5.Create();
+		byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(_myGUID + strword);
+		byte[] hash = md5.ComputeHash(inputBytes);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < hash.Length; i++)
+		{
+			sb.Append(hash[i].ToString("x2"));
+		}
+		return sb.ToString();
 	}
 }
-
