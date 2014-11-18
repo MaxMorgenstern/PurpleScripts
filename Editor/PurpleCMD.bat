e665613cd168fd07393eb328e2e57166f38c984e
Max <1@porzelt.net>
2014-11-18 23:41:00 +0100

    batch file update

--- Editor/PurpleCMD.bat
+++ Editor/PurpleCMD.bat
@@ -5,13 +5,12 @@ SET ProjectLocation=%~dp0..\..\..\..
 SET UnityLocation="D:\Unity\Editor\Unity.exe"
 
 ::Find Unity
-IF "%UnityLocation%" EQU "" (
-    echo TODO We have to search unity
+IF [%UnityLocation%] == [] (
+   for %%i in (Unity.exe) do SET UnityLocation=%%~$PATH:i
 )
 
-::for %i in (Explorer.exe) do @echo. %~$PATH:i
-
 ::Execution
-%UnityLocation% -batchmode -projectPath %ProjectLocation% -logFile %ProjectLocation%\log.txt -executeMethod PurpleMain.EntryPoint -quit -PurpleArguments:Example=%1;TestArg=%2
+%UnityLocation% -batchmode -nographics -projectPath %ProjectLocation% -logFile %ProjectLocation%\log.txt -executeMethod PurpleMain.EntryPoint -quit -PurpleArguments:Example=%1;TestArg=%2
+::%UnityLocation% -batchmode -nographics -projectPath %ProjectLocation% -logFile %ProjectLocation%\log.txt -executeMethod PurpleMain.EntryPoint -PurpleArguments:Example=%1;TestArg=%2
 
 pause
