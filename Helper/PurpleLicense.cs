f716a1770964cc1b57e78686f29d715ad5face99
Max <1@porzelt.net>
2014-11-22 19:48:02 +0100

    license class initial commit

---
+++ Helper/PurpleLicense.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+// http://msdn.microsoft.com/en-us/library/system.componentmodel.licenseprovider.aspx
+// http://stackoverflow.com/questions/2896888/developing-licenses-in-c-sharp-where-do-i-start
+
+public class PurpleLicense : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+}
+
