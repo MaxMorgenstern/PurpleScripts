08b542933c780b2061899f1469e82917bfa34e5e
Max <1@porzelt.net>
2014-11-24 17:37:04 +0100

    optimizations

--- Helper/PurpleLicense.cs
+++ Helper/PurpleLicense.cs
@@ -1,48 +1,36 @@
-using UnityEngine;
+using System;
 using System.Collections;
 using System.ComponentModel;
-
-
-using System;
 using System.IO;
-
-
-
-// http://msdn.microsoft.com/en-us/library/system.componentmodel.licenseprovider.aspx
-// http://stackoverflow.com/questions/2896888/developing-licenses-in-c-sharp-where-do-i-start
-using System.Security.Cryptography;
-using System.Security;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Security;
+using System.Security.Cryptography;
+using UnityEngine;
 
+// TODO : a lot!
 
 namespace PurpleLicense
 {
 	public class PurpleLicense : MonoBehaviour
 	{
-		public static License CreateLicense(DateTime start, DateTime end, String productName, String userName, String privateKey)
+		public static License CreateLicense (DateTime start, DateTime end, String productName, String userName, String privateKey)
 		{
-			// create the licence terms:
-			LicenseTerms terms = new LicenseTerms()
+			LicenseTerm terms = new LicenseTerm()
 			{
 				StartDate = start,
 				EndDate = end,
 				ProductName = productName,
 				UserName = userName
 			};
-			
-			// create the crypto-service provider:
+
 			DSACryptoServiceProvider dsa = new DSACryptoServiceProvider();
-			
-			// setup the dsa from the private key:
+
 			dsa.FromXmlString(privateKey);
-			
-			// get the byte-array of the licence terms:
+
 			byte[] license = terms.GetLicenseData();
-			
-			// get the signature:
+
 			byte[] signature = dsa.SignData(license);
 			
-			// now create the license object:
 			return new License()
 			{
 				LicenseTerms = Convert.ToBase64String(license),
@@ -50,26 +38,40 @@ namespace PurpleLicense
 			};
 		}
 
-		private static LicenseTerms GetValidTerms(License license, String publicKey)
+		public static LicenseTerm GetLicenseDetails (License license, String publicKey)
 		{
-			// create the crypto-service provider:
 			DSACryptoServiceProvider dsa = new DSACryptoServiceProvider();
-			
-			// setup the provider from the public key:
 			dsa.FromXmlString(publicKey);
 			
-			// get the license terms data:
 			byte[] terms = Convert.FromBase64String(license.LicenseTerms);
 			
-			// get the signature data:
 			byte[] signature = Convert.FromBase64String(license.Signature);
 			
-			// verify that the license-terms match the signature data
 			if (dsa.VerifyData(terms, signature))
-				return LicenseTerms.FromString(license.LicenseTerms);
+			{
+				return LicenseTerm.FromString(license.LicenseTerms);
+			}
 			else
+			{
 				throw new SecurityException("Signature Not Verified!");
+			}
 		}
+
+		public static bool ValidateLicense (License license, String publicKey)
+		{
+			return true;
+		}
+
+		public static LicenseTerm CreateTerm()
+		{
+			return new LicenseTerm (){};
+		}
+
+		public static bool ValidateKey()
+		{
+			return true;
+		}
+
 	}
 
 
@@ -78,73 +80,59 @@ namespace PurpleLicense
 	{
 		public string LicenseTerms;
 		public string Signature;
+		public string Key;
 	}
 
 	
 	[Serializable]
-	public class LicenseTerms
+	public class LicenseTerm
 	{
-		public DateTime StartDate { get; set; }
+		public DateTime StartDate;
+		public DateTime EndDate;
+		
+		public String Name;
+		public String Key;
 
-		public String UserName { get; set; }
-
-		public String ProductName { get; set; }
-
-		public DateTime EndDate { get; set; }
+		public String UserName;
+		public String ProductName;
 
 		public String GetLicenseString()
 		{
 			using (MemoryStream ms = new MemoryStream())
 			{
-				// create a binary formatter:
 				BinaryFormatter bnfmt = new BinaryFormatter();
-				
-				// serialize the data to the memory-steam;
 				bnfmt.Serialize(ms, this);
-				
-				// return a base64 string representation of the binary data:
 				return Convert.ToBase64String(ms.GetBuffer());
-				
 			}
 		}
-		
 
 		public byte[] GetLicenseData()
 		{
 			using (MemoryStream ms = new MemoryStream())
 			{
-				// create a binary formatter:
 				BinaryFormatter bnfmt = new BinaryFormatter();
-				
-				// serialize the data to the memory-steam;
 				bnfmt.Serialize(ms, this);
-				
-				// return a base64 string representation of the binary data:
-				return ms.GetBuffer();
-				
+				return ms.GetBuffer();	
 			}
 		}
-		
 
-		internal static LicenseTerms FromString(String licenseTerms)
+		internal static LicenseTerm FromString(String licenseTerms)
 		{
-			
 			using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(licenseTerms)))
 			{
-				// create a binary formatter:
 				BinaryFormatter bnfmt = new BinaryFormatter();
-				
-				// serialize the data to the memory-steam;
 				object value = bnfmt.Deserialize(ms);
 				
-				if (value is LicenseTerms)
-					return (LicenseTerms)value;
+				if (value is LicenseTerm)
+				{
+					return (LicenseTerm)value;
+				}
 				else
+				{
 					throw new ApplicationException("Invalid Type!");
-				
+				}
 			}
-		}
-		
+		}	
 	}
 }
 
