cb9f87ba0cdd5d3e47fbe778700e0f85f88c1f63
Max <1@porzelt.net>
2014-12-01 21:34:33 +0100

    Unit Test File added for future implementation

---
+++ Testing/UnitTest.cs
@@ -0,0 +1,35 @@
+/*
+ * 
+ * TODO : Unity Unit Tests
+ * 
+ */
+
+/*
+using System;
+using NUnit.Framework;
+using PurpleConfig;
+
+namespace PurpleTests
+{
+	[TestFixture()]
+	public class BasicTest
+	{
+		[Test()]
+		public void TestCase ()
+		{
+			Assert.AreEqual (20, 20);
+		}
+	}
+
+	[TestFixture()]
+	public class PurpleConfigTest
+	{
+		[Test()]
+		public void StringTest ()
+		{
+			bool b = PurpleConfig.ItemIds.getConfigBoolean ("test");
+			Assert.IsFalse ( b );
+		}
+	}
+}
+*/
