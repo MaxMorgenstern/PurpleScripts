a77fd63e501c8e4393ff118e60cf742fcd66b05c
Max <1@porzelt.net>
2014-12-16 00:42:19 +0100

    Initial Update

--- PurpleNetworkServer.cs
+++ PurpleNetworkServer.cs
@@ -17,103 +17,161 @@ using System.Collections;
 
 namespace PurpleNetwork
 {
-
-	public class PurpleServer : MonoBehaviour
+	namespace Server
 	{
-		public class Config
+		public enum ServerType { Account, Lobby, Game, Monitoring };
+
+		public class ServerConfig
 		{
-			public string todo;
-		}
+			public string name;
+			public string password;
+			public ServerType type;
+			public int maxUser;
+			public int port;
 
-		public enum ServerType { Account, /*Lobby,*/ Game, Multi, Monitoring };
-		private Config stdServerConfig;	
-
-		private static PurpleServer instance;
-
-
-		// START UP /////////////////////////
-		protected PurpleServer ()
-		{
-			stdServerConfig = new Config ();
-			// TODO
-			Debug.Log (stdServerConfig);
-		}
-
-
-		public static void LaunchServer()
-		{
-			LaunchServer (ServerType.Multi);
-		}
-
-		public static void LaunchServer(string type)
-		{
-			LaunchServer(Instance.parse_server_type (type));
-		}
-
-		public static void LaunchServer(ServerType type)
-		{
-			// TODO
-		}
-
-		public static void Setup()
-		{
-			// TODO: do server setup
-			// perhaps return an server setup option object
-		}
-
-
-		public static void StopServer()
-		{
-			StopServer (0);
-		}
-
-		public static void StopServer(float time)
-		{
-			// TODO: float is time in seconds
-		}
-
-
-		public static void RestartServer()
-		{
-			RestartServer (0);
-		}
-		
-		public static void RestartServer(float time)
-		{
-			// TODO: float is time in seconds
-		}
-		
-		
-		
-		private ServerType parse_server_type(string type)
-		{
-			return (ServerType) Enum.Parse(typeof(ServerType), type, true);
-		}
-
-		// Game List - or game instance list
-		// CreateGame(name, password, player, game, version, options, public);
-
-		// event handler
-
-		// interval update
-
-		// option autoritative
-
-
-
-
-		// SINGLETON /////////////////////////
-		public static PurpleServer Instance
-		{
-			get
+			// CONSTRUCTOR
+			public ServerConfig ()
 			{
-				if (instance == null)
-				{
-					GameObject gameObject 	= new GameObject ("PurpleServerManager");
-					instance     			= gameObject.AddComponent<PurpleServer> ();
-				}
-				return instance;
+				name = "GameServer";
+				password = "purple";
+				type = ServerType.Game;
+				maxUser = 32;
+				port = 1000;
 			}
+			public ServerConfig (string serverName, string serverPassword, ServerType serverType)
+			{
+				name = serverName;
+				password = serverPassword;
+				type = serverType;
+				maxUser = 32;
+				port = 1000;
+			}
+
+			public void SetType(string serverType)
+			{
+				SetType(parse_server_type (serverType));
+			}
+
+			public void SetType(ServerType serverType)
+			{
+				type = serverType;
+			}
+
+			// PRIVATE HELPER /////////////////////////
+			private ServerType parse_server_type(string serverType)
+			{
+				return (ServerType) Enum.Parse(typeof(ServerType), serverType, true);
+			}
+		}
+
+
+
+		public class PurpleServer : MonoBehaviour
+		{
+
+			private ServerConfig stdServerConfig;	
+
+			private static PurpleServer instance;
+
+
+			// START UP /////////////////////////
+			protected PurpleServer ()
+			{
+				stdServerConfig = new ServerConfig ();
+			}
+
+			
+			// SINGLETON /////////////////////////
+			public static PurpleServer Instance
+			{
+				get
+				{
+					if (instance == null)
+					{
+						GameObject gameObject 	= new GameObject ("PurpleServerManager");
+						instance     			= gameObject.AddComponent<PurpleServer> ();
+					}
+					return instance;
+				}
+			}
+
+
+			// PUBLIC FUNCTIONS /////////////////////////
+			public static void LaunchServer()
+			{
+				Instance.launch_server ();
+			}
+
+			public static void LaunchServer(ServerConfig config)
+			{
+				Instance.launch_server (config);
+			}
+
+
+
+			
+			private void launch_server()
+			{
+				launch_server (stdServerConfig);
+			}
+
+			private void launch_server(ServerConfig config)
+			{
+				int player = config.maxUser;
+				string password = config.password;
+				int port = config.port;
+				PurpleNetwork.LaunchLocalServer(player, password, port);
+			}
+
+/*
+			public static void LaunchServer(string type)
+			{
+				LaunchServer(Instance.parse_server_type (type));
+			}
+
+			public static void LaunchServer(ServerType type)
+			{
+				// TODO
+			}
+*/
+			// LaunchLocalServer (int player, string localPassword, int localPort)
+
+			public static void StopServer()
+			{
+				StopServer (0);
+			}
+
+			public static void StopServer(float time)
+			{
+				// TODO: float is time in seconds
+			}
+
+
+			public static void RestartServer()
+			{
+				RestartServer (0);
+			}
+			
+			public static void RestartServer(float time)
+			{
+				// TODO: float is time in seconds
+			}
+			
+
+
+
+
+			// Game List - or game instance list
+			// CreateGame(name, password, player, game, version, options, public);
+
+			// event handler
+
+			// interval update
+
+			// option autoritative
+
+
+
 		}
 	}
 }
