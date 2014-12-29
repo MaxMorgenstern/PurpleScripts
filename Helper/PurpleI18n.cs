490767210885bc1e690ce2de208146618087f291
Max <1@porzelt.net>
2014-12-29 01:03:33 +0100

    Initial I18n commit

---
+++ Helper/PurpleI18n.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Xml;
+using UnityEngine;
+
+public class PurpleI18n : MonoBehaviour
+{
+	private static PurpleI18n instance;
+
+	private static Dictionary<string,string> configDictionary;
+	private static CultureInfo currentCulture;
+	private static string defaultCulture;
+
+	// constructor - reads the language files and builds the dictionary
+	static PurpleI18n()
+	{
+		try{
+			defaultCulture = PurpleConfig.Globalization.Culture;
+		} catch(Exception e){
+			defaultCulture = "en-GB" ;
+			Debug.LogError("Can not read Purple Config! " + e.ToString());
+		}
+
+		configDictionary = new Dictionary<string,string>();
+		currentCulture = new CultureInfo( defaultCulture );
+	}
+
+	
+	// SINGLETON /////////////////////////
+	private static PurpleI18n Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				GameObject gameObject 	= new GameObject ("PurpleI18nManager");
+				instance     			= gameObject.AddComponent<PurpleI18n> ();
+				instance.setup();
+			}
+			return instance;
+		}
+	}
+
+	public static void Setup()
+	{
+		Instance.setup ();
+	}
+
+	public static void Setup(string culture)
+	{
+		Instance.setup (culture);
+	}
+
+	public static string Get(string key)
+	{
+		return Instance.get_entry (key.ToUpper());
+	}
+
+	public static CultureInfo GetCulture()
+	{
+		return Instance.get_culture ();
+	}
+	
+
+	// PRIVATE FUNCTIONS /////////////////////////
+	private void setup()
+	{
+		setup (defaultCulture);
+	}
+
+	private void setup(string culture)
+	{
+		try {
+			currentCulture = new CultureInfo (culture);
+			configDictionary = new Dictionary<string,string>();
+
+			XmlDocument xmlDoc = new XmlDocument(); 
+			
+			string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), 
+			                                    ("*"+culture+"*.lang"), SearchOption.AllDirectories);
+			if(files.Length > 0)
+			{
+				foreach (string filePath in files) 
+				{
+					xmlDoc.LoadXml (System.IO.File.ReadAllText(filePath)); // load the file.
+					XmlNodeList nodesList = xmlDoc.GetElementsByTagName("add"); // array of the level nodes.
+					foreach (XmlNode levelInfo in nodesList) 
+					{
+						configDictionary.Add(levelInfo.Attributes["key"].Value,levelInfo.Attributes["value"].Value);
+					}	
+				}
+			} else {
+				Debug.LogError("Can not find config files.");
+			}
+		} catch(Exception e) {
+			Debug.LogError(e);
+		}
+	}
+
+	private string get_entry(string entry)
+	{
+		string returnData = String.Empty;
+		try
+		{
+			configDictionary.TryGetValue(entry,out returnData);
+		}
+		catch (Exception e)
+		{
+			Debug.LogWarning(e.ToString());
+		}
+		return (String.IsNullOrEmpty(returnData)) ? "Missing Label: '"+entry+"'" : returnData;
+	}
+
+	private CultureInfo get_culture()
+	{
+		return currentCulture;
+	}
+}
