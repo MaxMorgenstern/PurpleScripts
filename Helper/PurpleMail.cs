cc20cd964a42bd6772aba136c34d5029a8dd101f
Max <1@porzelt.net>
2015-01-16 10:13:13 +0100

    SSL and Port added to mail client

--- Helper/PurpleMail.cs
+++ Helper/PurpleMail.cs
@@ -8,30 +8,26 @@ using UnityEngine;
 
 public class PurpleMail : MonoBehaviour
 {
-	private static PurpleMail instance;
+	private static PurpleMail 	instance;
 
-	private static string senderAddress;
-	private static string senderDisplayName;
-	private static MailAddress senderMail;
+	private static string 		senderAddress;
+	private static string 		senderDisplayName;
+	private static MailAddress 	senderMail;
 
+	private static string 		mailHost;
+	private static string 		mailUser;
+	private static string 		mailPassword;
+
+	private static int 			mailPort;
+	private static bool 		mailUseSSL;
+	
 	private static Dictionary<string,string> placeholderDictionary;
-
-	private static string mailHost;
-	private static string mailUser;
-	private static string mailPassword;
-
-	//private static int mailPort;
-	//private static bool mailSSL;
-
+	
 
 	// START UP /////////////////////////
 	protected PurpleMail ()
 	{
 		placeholderDictionary = new Dictionary<string,string>(StringComparer.OrdinalIgnoreCase);
-
-		// TODO
-		//mailPort = PurpleConfig.Mail.Server.Port;
-		//mailSSL = PurpleConfig.Mail.Server.SSL;
 
 		try {
 			senderAddress = PurpleConfig.Mail.Sender.Address;
@@ -41,6 +37,9 @@ public class PurpleMail : MonoBehaviour
 			mailUser = PurpleConfig.Mail.Server.User;
 			mailPassword = PurpleConfig.Mail.Server.Password;
 
+			mailPort = PurpleConfig.Mail.Server.Port;
+			mailUseSSL = PurpleConfig.Mail.Server.SSL;
+
 		} catch(Exception e){
 			senderAddress = "no-reply@example.com";
 			senderDisplayName = "No Reply";
@@ -48,6 +47,9 @@ public class PurpleMail : MonoBehaviour
 			mailHost = "localhost";
 			mailUser = String.Empty;
 			mailPassword = String.Empty;
+
+			mailPort = 25;
+			mailUseSSL = false;
 
 			Debug.LogError("Can not read Purple Config! " + e.ToString());
 		}
@@ -69,18 +71,6 @@ public class PurpleMail : MonoBehaviour
 	}
 
 	// PUBLIC FUNCTIONS /////////////////////////
-
-	public static bool Send()
-	{
-		// TODO: default values
-		string recipient = "1@porzelt.net";
-		string title = "Hallo, World!";
-		string body = "Hello, {DUMMY}!<br />This email is just a HTML test...<br /><b>This is a bold part!</b><br />";
-		Instance.add_dictionary_entry("DUMMY", "Max Mustermann");
-
-		return Instance.send_mail(recipient, title, body);
-	}
-
 	public static bool Send(string recipient, string title, string body)
 	{
 		return Instance.send_mail (recipient, title, body);
@@ -107,44 +97,27 @@ public class PurpleMail : MonoBehaviour
 		Instance.add_dictionary_entry (key, value);
 	}
 
-	
+
 	// TODO ...
+	/*
+	public static void EnableSigning()
+	{
+		Instance.enable_smime ();
+	}
+	
+	public static void DisableSigning()
+	{
+		Instance.disable_smime ();
+	}
+	
 	private void enable_smime()
 	{
-		/*
-		X509Store store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
-		store.Open(OpenFlags.ReadOnly);
-		X509Certificate2Collection certs = store.Certificates;
-		X509Certificate2 certificate = null;
-		foreach (X509Certificate2 cert in certs)
-		{
-			Debug.Log(cert.Subject);
-			if (cert.Subject.IndexOf("maximilian@porzelt.net") >= 0)
-			{
-				Debug.Log("found....");
-				certificate = cert;
-				break;
-			}
-		}
-		*/
-	
-		/*
-		try
-		{
-			// Find certificate by email adddress in My Personal Store.
-			// Once the certificate is loaded to From, the email content
-			// will be signed automatically                 
-			mail.From.Certificate.FindSubject(mail.From.Address,
-			                                   Certificate.CertificateStoreLocation.CERT_SYSTEM_STORE_CURRENT_USER,
-			                                   "My");
-		}
-		catch (Exception exp)
-		{
-			Debug.Log("No sign certificate found for <" + 
-			                  mail.From.Address + ">:" + exp.Message);
-		}
-		*/
 	}
+
+	private void disable_smime()
+	{
+	}
+	*/
 
 	
 	// PRIVATE FUNCTIONS /////////////////////////
@@ -177,6 +150,9 @@ public class PurpleMail : MonoBehaviour
 			
 			SmtpClient client = new SmtpClient(mailHost);
 			client.Credentials = new NetworkCredential(mailUser, mailPassword);
+
+			client.Port = mailPort;
+			client.EnableSsl = mailUseSSL;
 
 			client.Send(mail);
 			return true;
