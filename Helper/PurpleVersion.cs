5117db63f7dd4a2f97d6887cfc6f8f922863439b
Max <1@porzelt.net>
2015-01-29 17:11:58 +0100

    Versioning improved

--- Helper/PurpleVersion.cs
+++ Helper/PurpleVersion.cs
@@ -1,18 +1,20 @@
 using System;
+using System.Reflection;
 
+[assembly:AssemblyVersion ("1.0.*.*")]
 public class PurpleVersion
 {
 	// Version intormation
-	private static int 	_Major;		// Major Builds
-	private static int 	_Minor;		// Minor Builds - Functions added
-	private static int 	_Status;	// 0 for alpha - 1 for beta - 2 for release candidate - 3 for (final) release
-	private static int 	_Revision;	// Bugs fixed - changes made to code
+	private static int 	_Major;		// Major - Big Versions
+	private static int 	_Minor;		// Minor - Functions added
+	private static int 	_Build;		// Buildnumer - Days of development
+	private static int 	_Revision;	// Revision numbers
 
 	public PurpleVersion ()
 	{
 		_Major = 0;
 		_Minor = 0;
-		_Status = 0;
+		_Build = 0;
 		_Revision = 1;
 	}
 
@@ -20,7 +22,7 @@ public class PurpleVersion
 	{
 		_Major = major;
 		_Minor = minor;
-		_Status = status;
+		_Build = status;
 		_Revision = revision;
 	}
 
@@ -30,7 +32,7 @@ public class PurpleVersion
 	{
 		get
 		{
-			return _Major.ToString() +"."+ _Minor.ToString() +"."+ _Status.ToString() +"."+ _Revision.ToString();
+			return _Major.ToString() +"."+ _Minor.ToString() +"."+ _Build.ToString() +"."+ _Revision.ToString();
 		}
 	}
 
@@ -38,14 +40,18 @@ public class PurpleVersion
 	{
 		_Major = major;
 		_Minor = minor;
-		_Status = status;
+		_Build = status;
 		_Revision = revision;
 	}
 
 	public string GetCurrent()
 	{
-		// Return the current PurpleScripts version
-		PurpleVersion pv = new PurpleVersion (0, 3, 0, 1); // 2015-01-19
+		System.Version sv = Assembly.GetExecutingAssembly ().GetName ().Version;
+		int firstDevelopmentDay = 5500;
+
+		// Return the current PurpleScripts version	
+		// Major, Minor, Days of Development, Build minute of current day
+		PurpleVersion pv = new PurpleVersion (0, 3, (sv.Build-firstDevelopmentDay), (sv.Revision*2/60));
 		return pv.Version;
 	}
 }
