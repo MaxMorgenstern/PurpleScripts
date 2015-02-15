657866033da02c47901c00ea0e6dff05b3691555
Max <1@porzelt.net>
2015-02-15 21:58:58 +0100

    Renew Wrapper Class #1

--- Helper/PurpleDatabaseWrapper.cs
+++ Helper/PurpleDatabaseWrapper.cs
@@ -1,44 +1,104 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System;
 
 namespace PurpleDatabase
 {
-	public class Query
+	public static class SQLGenerator
 	{
 		private static List<string> SQLHistory;
 		private static string SQLQuery;
 
 		private class SQLQueryItem
 		{
-			public enum OptionEnum {SELECT, UPDATE, DELETE};
-			public enum SortEnum {ASC, DESC};
+			public enum TypeEnum {SELECT, UPDATE, DELETE};
+			public enum SortEnum {NONE, ASC, DESC};
+
+			// Variables
+			public TypeEnum 		Type		= TypeEnum.SELECT;
+			public List <string> 	Fields		= new List<string>();	
+
+			public string 			Table		= string.Empty;
+
+			public List <string> 	Filter		= null;
+
+			public int 				Limit		= 0;
+			public int 				Offset		= 0;
+
+			public SortEnum			Sorting 	= SortEnum.NONE;
+
+
+			// PRIVATE ////////////////////////////
+			private string 			query		= String.Empty;
 			
-			public string keyFrom 		= "FROM";
-			public string keyWhere 		= "WHERE";
-			public string keyLike 		= "LIKE";
-			public string keySet 		= "SET";
-			public string keyLimit 		= "LIMIT";
-			public string keyOffset 	= "OFFSET";
-			public string keyOrderBy 	= "ORDER BY";
-			public string keyStar 		= "*";
-			public string keyLikeSymbol	= "%";
-			public string keyEnd 		= ";";
+			private static string keyFrom 		= "FROM";
+			private static string keyWhere 		= "WHERE";
+			private static string keyLike 		= "LIKE";
+			private static string keySet 		= "SET";
+			private static string keyLimit 		= "LIMIT";
+			private static string keyOffset 	= "OFFSET";
+			private static string keyOrderBy 	= "ORDER BY";
+			private static string keyStar 		= "*";
+			private static string keyLikeSymbol	= "%";
+			private static string keyEnd 		= ";";
+			private static string keySpace 		= " ";
 
-			public OptionEnum 		Option;
 
-			public List <string> 	Fields;
-			public string 			Table;
-			public List <string> 	Logic;
+			// MAIN ////////////////////////////
+			public string build()
+			{
+				// TODO: smarter
 
-			public int 				Limit;
-			public int 				Offset;
+				query += Type.ToString () + keySpace + string.Join(", ", Fields.ToArray());
+				query += keySpace + keyFrom + keySpace + Table;
+				query += keyEnd;
 
-			public SortEnum			Sorting;
+				return query.Trim();
+			}
 
+
+			// SETUP - HELPER ////////////////////////////
+			public void SetSorting(string LoadOption)
+			{
+				Sorting = (SortEnum) Enum.Parse(typeof(SortEnum), LoadOption, true);
+			}
+			public void SetSorting(SortEnum LoadOption)
+			{
+				Sorting = LoadOption;
+			}
+
+		}
+		
+
+		public static string Select(string select = "*", string from = "", 
+					string where = "", int limit = 0, int offset = 0, string sorting = "") {
+
+			SQLQueryItem sqi = new SQLQueryItem ();
+			sqi.Fields.Add (select);
+			sqi.Table = from;
+
+			//TODO: where
+
+			sqi.Limit = limit;
+			sqi.Offset = offset;
+
+			if (!String.IsNullOrEmpty (sorting))
+				sqi.SetSorting (sorting); 
+
+			return sqi.build ();
+		}
+
+		public static DataTable Fetch(this string query)
+		{
+			return PurpleDatabase.SelectQuery (query);
 		}
 
 
 
+
+		
+		/*
 		Hashtable args = new Hashtable();
 		object this[string key]
 		{
@@ -51,8 +111,9 @@ namespace PurpleDatabase
 				args[key] = value;
 			}
 		}
-
-
+		*/
+		
+		
 		// TODO - lists for operations...
 		// OPTION
 		// FIELDS
@@ -60,7 +121,7 @@ namespace PurpleDatabase
 		// LOGIC
 		// LIMITATION
 		// SORTING
-
+		
 		/*
 
 		TableNameFromUserInput
@@ -75,13 +136,12 @@ namespace PurpleDatabase
 
 
 
-		protected Query ()
-		{
-			PurpleDatabase.Initialize ();
-		}
 
-		private void build_query()
+
+		/*
+		public static DataTable Fetch(string table)
 		{
+			return new DataTable ();
 		}
 
 
@@ -167,6 +227,6 @@ namespace PurpleDatabase
 		{
 			
 		}
-
+		*/
 	}
 }
