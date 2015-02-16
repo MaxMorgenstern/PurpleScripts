c60974ecf9b431ee9bd14265756deab4327fed6c
Max <1@porzelt.net>
2015-02-16 19:40:40 +0100

    Wrapper update

--- Helper/PurpleDatabaseWrapper.cs
+++ Helper/PurpleDatabaseWrapper.cs
@@ -9,293 +9,252 @@ namespace PurpleDatabase
 	{
 		private static List<string> _SQLHistory = new List<string>();
 		private static SQLQueryItem _SQLQuery;
+		
+		// SELECT - MASTER
+		public static string Select(string select = "*", string from = "",
+		                            string where = "", int limit = 0, int offset = 0, string sorting = "")
+		{
+			_SQLQuery = new SQLQueryItem();
+			_SQLQuery.Fields.Add(select);
+			
+			if (!String.IsNullOrEmpty(from))
+				_SQLQuery.Table = from;
 
+			if(!String.IsNullOrEmpty(where))
+				_SQLQuery.set_filter(where);
+
+			if(limit != 0)
+				_SQLQuery.Limit = limit;
+
+			if(offset != 0)
+				_SQLQuery.Offset = offset;
+			
+			if (!String.IsNullOrEmpty(sorting))
+				_SQLQuery.set_order_by(sorting);
+			
+			return _SQLQuery.build();
+		}
+		
+		// UPDATE - MASTER
+		public static string Update()
+		{
+			// TODO
+			return String.Empty;
+		}
+		
+		// DELETE - MASTER
+		public static string Delete()
+		{
+			// TODO
+			return String.Empty;
+		}
+		
+		
+		// SINGLE OPERATIONS /////////////
+		
+		// NEW QUERY
+		public static void New()
+		{
+			_SQLQuery = new SQLQueryItem();
+		}
+		
+		public static void Reset()
+		{
+			_SQLQuery = new SQLQueryItem();
+		}
+
+		
+		// FROM
+		public static void From(string table)
+		{
+			_SQLQuery.Table = table;
+		}
+		
+		// WHERE
+		public static void Where(string logic)
+		{
+			_SQLQuery.set_filter(logic);
+		}
+		
+		// LIMIT - OFFSET
+		public static void Limit(int limit = 0, int offset = 0)
+		{
+			_SQLQuery.Limit = limit;
+			_SQLQuery.Offset = offset;
+		}
+		
+		// ORDER BY
+		public static void OrderBy(string SortOption)
+		{
+			_SQLQuery.set_order_by(SortOption);
+		}
+		
+		public static void OrderBy(string SortField, string SortOrder)
+		{
+			_SQLQuery.set_order_by(SortField, SortOrder);
+		}
+		
+		
+		// EXECUTE QUERY
+		public static DataTable Execute(this string query)
+		{
+			_SQLHistory.Add(query);
+			return PurpleDatabase.SelectQuery(query);
+		}
+		
+		public static DataTable Fetch(this string query)
+		{
+			_SQLHistory.Add(query);
+			return PurpleDatabase.SelectQuery(query);
+		}
+		
+		
+		
+		
+		
+		/*
+        public static void Like() {}
+        public static void ASC() {}
+        public static void DESC() {}
+        public static void Set() {}
+        */
+		
+		
 		private class SQLQueryItem
 		{
-			public enum TypeEnum {SELECT, UPDATE, DELETE};
-			public enum SortEnum {NONE, ASC, DESC};
-
+			public enum TypeEnum { SELECT, UPDATE, DELETE };
+			public enum SortEnum { NONE, ASC, DESC };
+			
 			// Variables
-			public TypeEnum 		Type		= TypeEnum.SELECT;
-			public List <string> 	Fields		= new List<string>();	
-
-			public string 			Table		= string.Empty;
-
-			public List <string> 	Filter		= null;
+			public TypeEnum Type 			= TypeEnum.SELECT;
+			public List<string> Fields 		= new List<string>();
 			
-			public Dictionary<string, SortEnum> SortList 
-				= new Dictionary<string, SortEnum> ();
-
-			public int 				Limit		= 0;
-			public int 				Offset		= 0;
-
-
+			public string Table 			= string.Empty;
+			
+			public List<string> Filter 		= new List<string>();
+			
+			public Dictionary<string, SortEnum> SortList
+							= new Dictionary<string, SortEnum>();
+			
+			public int Limit 				= 0;
+			public int Offset				= 0;
+			
+			
 			// PRIVATE ////////////////////////////
-			private string 			query		= String.Empty;
+			private string query 					= String.Empty;
 			
-			private static string keyFrom 		= "FROM";
-			private static string keyWhere 		= "WHERE";
+			private static string keyFrom 			= "FROM";
+			private static string keyWhere 			= "WHERE";
 			// private static string keyLike 		= "LIKE";
 			// private static string keySet 		= "SET";
-			private static string keyLimit 		= "LIMIT";
-			private static string keyOffset 	= "OFFSET";
-			private static string keyOrderBy 	= "ORDER BY";
-			private static string keyStar 		= "*";
+			private static string keyLimit 			= "LIMIT";
+			private static string keyOffset 		= "OFFSET";
+			private static string keyOrderBy 		= "ORDER BY";
+			private static string keyStar 			= "*";
 			// private static string keyLikeSymbol	= "%";
-			private static string keyEnd 		= ";";
-			private static string keySpace 		= " ";
-
-
+			private static string keyEnd 			= ";";
+			private static string keySpace			 = " ";
+			
+			
 			// MAIN ////////////////////////////
+			// Cheatsheet:
+			/*
+                SELECT * FROM table_name;
+                INSERT INTO table_name 
+                    VALUES (value1,value2,value3,...);
+                UPDATE table_name
+                    SET column1=value1,column2=value2,...
+                    WHERE some_column=some_value;
+                DELETE FROM table_name
+                    WHERE some_column=some_value;
+            */
+			
 			public string build()
 			{
 				// TYPE
-				Add (Type.ToString ());
+				Add(Type.ToString());
 				Fields.RemoveAll(x => x == keyStar);
-				if(Fields.Count > 0)
+				if (Fields.Count > 0)
 				{
-					Add (string.Join(", ", Fields.ToArray()));
+					Add(string.Join(", ", Fields.ToArray()));
 				}
 				else
 				{
-					Add (keyStar);
+					Add(keyStar);
 				}
-
+				
 				// TABLE
-				Add (keyFrom);
-				Add (Table);
-
-				// FILTER
-				if(Filter != null)
+				Add(keyFrom);
+				Add(Table);
+				
+				// FILTER / WHERE
+				if (Filter.Count > 0)
 				{
-					Add (keyWhere);
-					// TODO
+					Add(keyWhere);
+					// TODO - Not only AND
+					Add(string.Join("AND ", Fields.ToArray()));
 				}
-
+				
 				// SORTING
-				if(SortList.Count > 0)
+				if (SortList.Count > 0)
 				{
-					Add (keyOrderBy);
-
+					Add(keyOrderBy);
+					
 					foreach (KeyValuePair<string, SortEnum> SortElement in SortList)
 					{
-						if(SortElement.Value != SortEnum.NONE)
+						if (SortElement.Value != SortEnum.NONE)
 						{
-							Add (SortElement.Key);
-							Add (SortElement.Value.ToString());
+							Add(SortElement.Key);
+							Add(SortElement.Value.ToString());
 						}
 					}
 				}
-
+				
 				// LIMITATION
-				if(Limit != 0)
+				if (Limit != 0)
 				{
-					Add (keyLimit);
-					Add (Limit.ToString());
+					Add(keyLimit);
+					Add(Limit.ToString());
 				}
 				
-				if(Offset != 0)
+				if (Offset != 0)
 				{
-					Add (keyOffset);
-					Add (Offset.ToString());
+					Add(keyOffset);
+					Add(Offset.ToString());
 				}
-
-				return query.Trim () + keyEnd;
+				
+				return query.Trim() + keyEnd;
 			}
-
-
+			
+			
 			// HELPER ////////////////////////////
-			public void SetSorting(string SortOption)
+			public void set_filter(string SortOption)
 			{
-				string [] split = SortOption.Split(new Char [] {' ', ',' });
-				for(int i=0; i<split.Length; i+=2)
+				Filter.Add (SortOption);
+			}
+
+				
+			public void set_order_by(string SortOption)
+			{
+				string[] split = SortOption.Split(new Char[] { ' ', ',' });
+				for (int i = 0; i < split.Length; i += 2)
 				{
-					SortList.Add(split[i], (SortEnum) Enum.Parse(typeof(SortEnum), split[i+1], true));
+					SortList.Add(split[i], (SortEnum)Enum.Parse(typeof(SortEnum), split[i + 1], true));
 				}
 			}
-
-			public void SetSorting(string SortField, string SortOrder)
+			
+			public void set_order_by(string SortField, string SortOrder)
 			{
-				SortList.Add(SortField, (SortEnum) Enum.Parse(typeof(SortEnum), SortOrder, true));
+				SortList.Add(SortField, (SortEnum)Enum.Parse(typeof(SortEnum), SortOrder, true));
 			}
-
-
+			
+			
 			// PRIVATE ////////////////////////////
 			private string Add(string part)
 			{
-				query += (keySpace + part).TrimEnd ();
+				query += (keySpace + part).TrimEnd();
 				return query;
 			}
 		}
-		
-
-		public static string Select(string select = "*", string from = "", 
-					string where = "", int limit = 0, int offset = 0, string sorting = "") {
-
-			_SQLQuery = new SQLQueryItem ();
-			_SQLQuery.Fields.Add (select);
-			_SQLQuery.Table = from;
-
-			//TODO: where
-
-			_SQLQuery.Limit = limit;
-			_SQLQuery.Offset = offset;
-
-			if (!String.IsNullOrEmpty (sorting))
-				_SQLQuery.SetSorting (sorting); 
-
-			return _SQLQuery.build ();
-		}
-		
-		public static void Sort(string SortOption)
-		{
-			_SQLQuery.SetSorting (SortOption);
-		}
-
-		public static void Sort(string SortField, string SortOrder)
-		{
-			_SQLQuery.SetSorting (SortField, SortOrder);	
-		}
-
-		public static DataTable Fetch(this string query)
-		{
-			_SQLHistory.Add (query);
-			return PurpleDatabase.SelectQuery (query);
-		}
-
-
-
-
-		
-		/*
-		Hashtable args = new Hashtable();
-		object this[string key]
-		{
-			get
-			{
-				return args[key];
-			}
-			set 
-			{
-				args[key] = value;
-			}
-		}
-		*/
-		
-		
-		// TODO - lists for operations...
-		// OPTION
-		// FIELDS
-		// TABLE
-		// LOGIC
-		// LIMITATION
-		// SORTING
-		
-		/*
-
-		TableNameFromUserInput
-
-		string mysqlstring = @"Unescaped but with at";
-
-		Idea:
-		dbConnection.select("age").from("user").where("name = 'Max'").execute();	SELECT age FROM user WHERE name = 'Max';
-		dbConnection.from("account").execute();										SELECT * FROM account;
-		dbConnection.sql("SELECT * FROM dummy LIMIT 5;");
-		 */
-
-
-
-
-
-		/*
-		public static DataTable Fetch(string table)
-		{
-			return new DataTable ();
-		}
-
-
-
-		// new query
-		public static void New()
-		{
-			
-		}
-
-		// execute query
-		public static void Execute()
-		{
-			
-		}
-
-		// sql query
-		public static void SQL()
-		{
-			
-		}
-		
-		
-		public static void Select()
-		{
-			
-		}
-		
-		public static void From()
-		{
-			
-		}
-		
-		public static void Where()
-		{
-			
-		}
-
-		public static void Like()
-		{
-			
-		}
-		
-		public static void Limit()
-		{
-			
-		}
-		
-		public static void Offset()
-		{
-			
-		}
-
-		public static void OrderBy()
-		{
-			
-		}
-
-		public static void ASC()
-		{
-			
-		}
-
-		public static void DESC()
-		{
-			
-		}
-		
-
-
-		public static void Update()
-		{
-			
-		}
-		
-		public static void Set()
-		{
-			
-		}
-		
-		
-		public static void Delete()
-		{
-			
-		}
-		*/
 	}
 }
