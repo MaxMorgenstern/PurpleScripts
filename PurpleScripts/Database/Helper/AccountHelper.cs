1655851aa6eda0d23aeeca87e8f404e3d81e0af4
Max <1@porzelt.net>
2015-03-21 13:53:20 +0100

    Use Entities instead of DataTable

--- PurpleScripts/Database/Helper/AccountHelper.cs
+++ PurpleScripts/Database/Helper/AccountHelper.cs
@@ -7,6 +7,11 @@ using UnityEngine;
 using Entities.PurpleNetwork;
 using PurpleDatabase.Extension;
 using PurpleNetwork.Server;
+using Entities.Database;
+using System.Collections.Generic;
+
+// TODO: update reference - update database
+// update PurpleNetworkUser
 
 namespace PurpleDatabase.Helper
 {
@@ -18,92 +23,95 @@ namespace PurpleDatabase.Helper
 
 		public static bool ValidateAuthentication(string identifier, string password="", string token="")
 		{
-			DataTable userData = get_user (identifier);
-			if(userData.Rows.Count == 0)
+			PurpleAccount userData = get_user (identifier);
+			if(userData == null)
 				return false;
 
 			if(!string.IsNullOrEmpty(password))
 			{
 				PurplePassword pp = new PurplePassword();
-				if(pp.ValidatePassword(password, userData.Rows[0]["password"].ToString()))
+				if(pp.ValidatePassword(password, userData.password))
 				{
 					return true;
 				}
 			}
 			else if (!string.IsNullOrEmpty(token))
 			{
-				if(token == userData.Rows[0]["token"].ToString())
+				if(!String.IsNullOrEmpty(userData.token) && token == userData.token)
 				{
 					return true;
 				}
 			}
-
 			return false;
 		}
 
-		// TODO
-		public static bool Register(string todo)
+		// TODO - Test
+		public static bool Register(PurpleAccount account)
 		{
-			string testusername = "testbuddy";
-			string testpassword = "helloworld";
-
-			if (PurpleAccountValidator.ValidatePasswordStrength (testpassword) 
-			    	&& PurpleAccountValidator.ValidateUsername (testusername))
-			{
-
-			}
-
-			// TODO: usernanme regex + password regex
-			/*
-			Username
-			password
-			email
-			firstname
-			lastname
-			gender
-			birthday
-			country code
-			language
-			 */
-			return false;
-		}
-
-		// TODO
-		public static bool Delete(string todo)
-		{
-			return false;
-		}
-
-
-		public static bool Login(string username, string password)
-		{
-			string token = string.Empty;
-			return Login (username, password, out token);
-		}
-
-		public static bool Login(string username, string password, out string token)
-		{
-			// TODO
-			token = String.Empty;
-
-			DataTable userData = get_user (username);
-			if(userData.Rows.Count == 0)
+			if(!IsUniqueUsername(account.username))
 				return false;
 
-			PurplePassword pp = new PurplePassword();
-			if(pp.ValidatePassword(password, userData.Rows[0]["password"].ToString()))
+			if (PurpleAccountValidator.ValidatePasswordStrength (account.password) 
+			    	&& PurpleAccountValidator.ValidateUsername (account.username))
 			{
-				// TODO: token
-				return true;
+				int affectedRows = account.ToSQLInsert().Execute();
+				return (affectedRows == 1) ? true : false;
 			}
-
-			// TODO: generate token set and update
-
-			token = userData.Rows[0]["token"].ToString();
 			return false;
 		}
+
+		// TODO - Test
+		public static bool IsUniqueUsername(string username)
+		{
+			PurpleAccount userData = get_database_user(username).ToList<PurpleAccount>().FirstOrDefault();
+			if(userData != null)
+				return false;
+			return true;
+		}
+
+		// TODO - Test
+		public static bool Disable(string identifier, string password)
+		{
+			if(!ValidateAuthentication (identifier, password))
+				return false;
+
+			PurpleAccount userData = get_user (identifier);
+			if(userData == null)
+				return false;
+
+			userData.active = false;
+			// TODO
+			return false;
+			//return (affectedRows == 1) ? true : false;
+		}
+
+		// TODO - Test
+		public static bool Login(string identifier, string password)
+		{
+			string token = string.Empty;
+			return Login (identifier, password, out token);
+		}
+
+		public static bool Login(string identifier, string password, out string token)
+		{
+			token = String.Empty;
+			if(!ValidateAuthentication (identifier, password))
+				return false;
+
+			PurpleAccount userData = get_user (identifier);
+			if(userData == null)
+				return false;
+
+			userData.token = PurpleHash.Token ();
+			token = userData.token;
+			userData.token_created = DateTime.Now;
+
+			// TODO
+
+			return true;
+		}
 		
-		public static bool Logout(string username)
+		public static bool Logout(string identifier)
 		{
 			/*
 			string query = SQLGenerator.Update ("token=null", "Account",  "username = `"+username+"`");
@@ -114,23 +122,23 @@ namespace PurpleDatabase.Helper
 			return false;
 		}
 	
-		public static bool AddWarning(string username, string comment, int level)
+		public static bool AddWarning(string identifier, string comment, int level)
 		{
-			DataTable userData = get_user (username);
-			if(userData.Rows.Count == 0)
+			PurpleAccount userData = get_user (identifier);
+			if(userData == null)
 				return false;
 			
-			int result = add_database_user_warning ((int)userData.Rows [0] ["id"], level, comment);
+			int result = add_database_user_warning (userData.id, level, comment);
 			return (result == 1) ? true : false;
 		}
 
-		public static bool AddLog(string username, string comment)
+		public static bool AddLog(string identifier, string comment)
 		{
-			DataTable userData = get_user (username);
-			if(userData.Rows.Count == 0)
+			PurpleAccount userData = get_user (identifier);
+			if(userData == null)
 				return false;
 			
-			int result = add_database_user_log ((int)userData.Rows [0] ["id"], comment);
+			int result = add_database_user_log (userData.id, comment);
 			return (result == 1) ? true : false;
 		}
 
@@ -142,75 +150,64 @@ namespace PurpleDatabase.Helper
 		// PRIVATE ////////////////////////////
 
 		// TODO: guid
-		private static DataTable get_user(string identifier)
+		private static PurpleAccount get_user(string identifier)
 		{
 			PurpleNetworkUser pnu = PurpleServer.UserList.Find (x => x.UserName == identifier || x.UserGUID.Equals(identifier));
 			if(pnu != null)
 			{
-				return pnu.GetAsDataTable ();
+				return pnu.GetAsDataTable ().ToList<PurpleAccount>().FirstOrDefault();
 			}
 			else
 			{
-				return get_database_user (identifier);
+				return get_database_user (identifier).ToList<PurpleAccount>().FirstOrDefault();
 			}
 		}
 
 		private static DataTable get_database_user(string identifier)
 		{
-			return SQLGenerator.New ().Select ("id, username, password, token, token_created").From (accountTable)
+			return SQLGenerator.New ().Select ("id, guid, username, password, token, token_created").From (accountTable)
 				.Where ("username=" + identifier).Where ("active=1")
-					.Where ("GUID=" + identifier, "OR").Where ("active=1").Single ().Fetch ();
+					.Where ("guid=" + identifier, "OR").Where ("active=1").Single ().Fetch ();
 		}
 
-		private static bool update_database_user()
+
+
+
+
+
+
+
+		private static bool update_network_user(PurpleAccount user)
 		{
-			// TODO
-			return true;
+			return false;
 		}
 
-		private static bool create_database_user()
+		// TODO - Test
+		private static bool update_database_user(PurpleAccount user)
 		{
-			// TODO
-			SQLGenerator.Insert ("account", "GUID, username, password, email, account_created, first_name, last_name, gender, birthday, country_code, language_code, account_type, active")
-				.Values ("");
-			/*
-// Create Account
-INSERT INTO `account` (`GUID`, `username`, `password`, `email`, `email_verification`, `account_created`, `timestamp`, `first_name`, `last_name`, `gender`, `birthday`, `country_code`, `language_code`, `last_seen`, `last_login`, `last_login_ip`, `account_type`, `token`, `token_created`, `comment`, `active`)
-VALUES ('INSERT GUID HERE', 'INSERTUSERNAME', 'PASSWORDHASH', 'MAILADDRESS', NULL, now(), now(), 'INSERTNAME', 'INSERTNAME2', 1, '1990-02-02', 'de', 'de', now(), NULL, NULL, 1, NULL, NULL, NULL, '1');
-		 */
-			return true;
+			int result = user.ToSQLUpdate ().Execute ();
+			return (result==1) ? true : false;
 		}
-		
-		private static int disable_database_user(int account_id)
+
+		private static bool create_database_user(PurpleAccount user)
 		{
-			// UPDATE `account` SET `active` = '0' WHERE `id` = '2';
-			return SQLGenerator.New ().Update ("active = 0", "account", "id=" + account_id).Execute ();
+			int result = user.ToSQLInsert ().Execute ();
+			return (result==1) ? true : false;
 		}
-		
-		private static int enable_database_user(int account_id)
-		{
-			// UPDATE `account` SET `active` = '1' WHERE `id` = '2';
-			return SQLGenerator.New ().Update ("active = 1", "account", "id=" + account_id).Execute ();
-		}
-		
-		
-		private static int update_database_user_token(int account_id, string token)
-		{
-			// UPDATE `account` SET `token` = 'SETTOKEN', `token_created` = now() WHERE `id` = '2';
-			return SQLGenerator.New ().Update ("token=" + token, "account", "id=" + account_id).Update ("token_created = now()").Execute ();
-		}
+
+
+
+
 
 
 		private static int add_database_user_warning(int account_id, int warning_level, string comment)
 		{
-			// INSERT INTO `account_warnings` (`account_id`, `warning_level`, `comment`, `timestamp`) VALUES ('2', '1', 'Reason for warning', now());
-			return SQLGenerator.New ().Insert("account_warnings", "account_id, warning_level, comment, timestamp").Values(account_id+", "+warning_level+", "+comment+", now()").Execute();
+			return SQLGenerator.New ().Insert(accountWarningsTable, "account_id, warning_level, comment, timestamp").Values(account_id+", "+warning_level+", "+comment+", now()").Execute();
 		}
 
 		private static int add_database_user_log(int account_id, string comment)
 		{
-			// INSERT INTO `account_log` (`account_id`, `log`, `timestamp`) VALUES ('1', 'Logdata', now());
-			return SQLGenerator.New ().Insert ("account_log", "account_id, log, timestamp").Values (account_id+", "+comment+", now()").Execute();
+			return SQLGenerator.New ().Insert (accountLogTable, "account_id, log, timestamp").Values (account_id+", "+comment+", now()").Execute();
 		} 
 
 	}
