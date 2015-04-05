a6dca56779ff34451b8b33260127733b286e342d
Max <1@porzelt.net>
2015-04-05 10:31:16 +0200

    Server Sanity Test added

--- PurpleScripts/Network/Server/PurpleNetworkServer.cs
+++ PurpleScripts/Network/Server/PurpleNetworkServer.cs
@@ -17,21 +17,21 @@ namespace PurpleNetwork.Server
 		private static ServerConfig stdServerConfig;
 		private static int stdServerdelay;
 		private static List<int> stdNotificationIntervalList;
-		
+
 		private static ServerConfig currentServerConfig;
-		
+
 		private static string restartNotificationMessage;
 		private static string restartNotificationDoneMessage;
 		private static string shutdownNotificationMessage;
 		private static string shutdownNotificationDoneMessage;
 		private static List <int> notificationIntervalList;
 		private static string notificationPlaceholder;
-		
+
 		private static PurpleCountdown countdown;
-		
+
 		private List<PurpleNetworkUser> userList;
-		
-		
+
+
 		// START UP /////////////////////////
 		protected PurpleServer ()
 		{
@@ -40,11 +40,11 @@ namespace PurpleNetwork.Server
 			stdNotificationIntervalList = new List<int> (
 				new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 30, 60, 300, 600, 900, 1800});
 			userList = new List<PurpleNetworkUser> ();
-			
+
 			try{
 				notificationPlaceholder = PurpleConfig.Network.Message.Placeholder;
 				stdServerdelay = PurpleConfig.Network.Server.Delay;
-				
+
 				shutdownNotificationMessage = PurpleI18n.Get("SHUTDOWN");
 				shutdownNotificationDoneMessage = PurpleI18n.Get("SHUTDOWNNOW");
 				restartNotificationMessage = PurpleI18n.Get("RESTART");
@@ -52,17 +52,17 @@ namespace PurpleNetwork.Server
 			} catch(Exception e){
 				notificationPlaceholder = "!!time!!";
 				stdServerdelay = 10;
-				
+
 				shutdownNotificationMessage = "Server shutdown in !!time!!!";
 				shutdownNotificationDoneMessage = "The server will be shut down now!";
 				restartNotificationMessage = "Server restart in !!time!!!";
 				restartNotificationDoneMessage = "The server will be restarted now!";
-				
+
 				Debug.LogError("Can not read Purple Config! " + e.ToString());
 			}
 		}
-		
-		
+
+
 		// SINGLETON /////////////////////////
 		private static PurpleServer Instance
 		{
@@ -77,7 +77,7 @@ namespace PurpleNetwork.Server
 				return instance;
 			}
 		}
-		
+
 		public static ServerConfig CurrentConfig
 		{
 			get
@@ -85,7 +85,7 @@ namespace PurpleNetwork.Server
 				return Instance.get_currnet_server_config();
 			}
 		}
-		
+
 		public static List<PurpleNetworkUser> UserList
 		{
 			get
@@ -97,170 +97,175 @@ namespace PurpleNetwork.Server
 				Instance.userList = value;
 			}
 		}
-		
-		
+
+
 		// PUBLIC FUNCTIONS /////////////////////////
 		public static void LaunchServer()
 		{
 			Instance.launch_server ();
 		}
-		
+
 		public static void LaunchServer(ServerConfig config)
 		{
 			Instance.launch_server (config);
 		}
-		
-		
+
+
 		public static void StopServer()
 		{
 			Instance.stop_server ();
 		}
-		
+
 		public static void StopServer(string message, string doneMessage)
 		{
 			Instance.stop_server (message, doneMessage);
 		}
-		
+
 		public static void StopServer(int seconds)
 		{
 			Instance.stop_server (seconds);
 		}
-		
+
 		public static void StopServer(int seconds, string message, string doneMessage)
 		{
 			Instance.stop_server (seconds, message, doneMessage);
 		}
-		
-		
+
+
 		public static void RestartServer()
 		{
 			Instance.restart_server ();
 		}
-		
+
 		public static void RestartServer(string message, string doneMessage)
 		{
 			Instance.restart_server (message, doneMessage);
 		}
-		
+
 		public static void RestartServer(int seconds)
 		{
 			Instance.restart_server (seconds);
 		}
-		
+
 		public static void RestartServer(int seconds, string message, string doneMessage)
 		{
 			Instance.restart_server (seconds, message, doneMessage);
 		}
-		
-		
+
+
 		public static void SetNotificationInterval(int interval)
 		{
 			List<int> intervalList = new List<int> ();
 			intervalList.Add (interval);
 			Instance.set_notification_interval (intervalList);
 		}
-		
+
 		public static void SetNotificationKeyword(string keyword)
 		{
 			Instance.set_notification_keyword (keyword);
 		}
-		
+
 		public static void SetNotificationInterval()
 		{
 			Instance.set_notification_interval ();
 		}
-		
+
 		public static void SetNotificationInterval(List <int> interval)
 		{
 			Instance.set_notification_interval (interval);
 		}
-		
+
 		public static List <int> GetNotificationInterval()
 		{
 			return Instance.get_notification_interval ();
 		}
-		
-		
-		
+
+
+
 		// PRIVATE FUNCTIONS /////////////////////////
 		// START
 		private void launch_server()
 		{
 			launch_server (currentServerConfig);
 		}
-		
+
 		private void launch_server(ServerConfig config)
 		{
 			currentServerConfig = config;
 			initialize_database_connection (currentServerConfig);
-			
+
 			PurpleNetwork.LaunchLocalServer(config.ServerMaxClients, config.ServerPassword, config.ServerPort);
 			PurpleNetwork.SetSpamProtection (config.SpamPrevention);
 			PurpleNetwork.SetSpamResponse (config.SpamResponse);
-			
+
 			switch (config.ServerType)
 			{
 			case ServerTypes.Account:
 				Handler.Handler.RegisterAccountListener();
 				break;
-				
+
 			case ServerTypes.Game:
 				Handler.Handler.RegisterGameListener();
 				break;
-				
+
 			case ServerTypes.Lobby:
 				Handler.Handler.RegisterLobbyListener();
 				break;
-				
+
 			case ServerTypes.Monitoring:
 				Handler.Handler.RegisterLMonitoringListener();
 				break;
-				
+
 			case ServerTypes.Multi:
 				Handler.Handler.RegisterMultiListener();
 				break;
 			}
+
+			if(config.ServerSanityTest)
+			{
+				PurpleNetworkServerSanityTester.ServerSanityCheck();
+			}
 		}
-		
-		
+
+
 		// STOP
 		private void stop_server()
 		{
 			stop_server (stdServerdelay);
 		}
-		
+
 		private void stop_server(string message, string doneMessage)
 		{
 			stop_server (stdServerdelay, message, doneMessage);
 		}
-		
+
 		private void stop_server(int seconds)
 		{
 			stop_server (seconds, shutdownNotificationMessage, shutdownNotificationDoneMessage);
 		}
-		
+
 		private void stop_server(int seconds, string message, string doneMessage)
 		{
 			shutdownNotificationMessage = message;
 			shutdownNotificationDoneMessage = doneMessage;
-			
+
 			countdown = PurpleCountdown.NewInstance ();
 			countdown.CountdownDoneEvent += stop_server_done;
 			countdown.CountdownRunEvent += stop_server_run;
 			countdown.CountDown (seconds);
 		}
-		
+
 		private void stop_server_run()
 		{
 			int time_left = countdown.CountDownLeft;
-			
+
 			if(notificationIntervalList.Contains(time_left))
 			{
 				PurpleNetwork.Broadcast ("server_broadcast",
 				                         create_broadcast_message(combine_notification_message(shutdownNotificationMessage, time_left)));
 			}
 		}
-		
+
 		private void stop_server_done()
 		{
 			countdown.CountdownDoneEvent -= stop_server_done;
@@ -269,46 +274,46 @@ namespace PurpleNetwork.Server
 			PurpleNetwork.Broadcast ("server_broadcast", create_broadcast_message(shutdownNotificationDoneMessage));
 			PurpleNetwork.StopLocalServer ();
 		}
-		
-		
+
+
 		// RESTART
 		private void restart_server()
 		{
 			restart_server (stdServerdelay);
 		}
-		
+
 		private void restart_server(string message, string doneMessage)
 		{
 			restart_server (stdServerdelay, message, doneMessage);
 		}
-		
+
 		private void restart_server(int seconds)
 		{
 			restart_server (seconds, restartNotificationMessage, restartNotificationDoneMessage);
 		}
-		
+
 		private void restart_server(int seconds, string message, string doneMessage)
 		{
 			restartNotificationMessage = message;
 			restartNotificationDoneMessage = doneMessage;
-			
+
 			countdown = PurpleCountdown.NewInstance ();
 			countdown.CountdownDoneEvent += restart_server_done;
 			countdown.CountdownRunEvent += restart_server_run;
 			countdown.CountDown (seconds);
 		}
-		
+
 		private void restart_server_run()
 		{
 			int time_left = countdown.CountDownLeft;
-			
+
 			if(notificationIntervalList.Contains(time_left))
 			{
 				PurpleNetwork.Broadcast ("server_broadcast",
 				                         create_broadcast_message(combine_notification_message(restartNotificationMessage, time_left)));
 			}
 		}
-		
+
 		private void restart_server_done()
 		{
 			countdown.CountdownDoneEvent -= restart_server_done;
@@ -317,75 +322,75 @@ namespace PurpleNetwork.Server
 			PurpleNetwork.Broadcast ("server_broadcast", create_broadcast_message(restartNotificationDoneMessage));
 			PurpleNetwork.RestartLocalServer ();
 		}
-		
+
 		// NOTIFICATION MESSAGE
 		private void set_notification_keyword(string keyword)
 		{
 			notificationPlaceholder = keyword;
 		}
-		
-		
+
+
 		// SET NOTIFICATION INTERVAL
 		private void set_notification_interval()
 		{
 			notificationIntervalList = stdNotificationIntervalList;
 		}
-		
+
 		private void set_notification_interval(List <int> interval)
 		{
 			notificationIntervalList = interval;
 		}
-		
+
 		// GET NOTIFICATION INTERVAL
 		private List <int> get_notification_interval()
 		{
 			return notificationIntervalList;
 		}
-		
+
 		private ServerConfig get_currnet_server_config()
 		{
 			return currentServerConfig;
 		}
-		
-		
+
+
 		// HELPER ////////////////////
 		private string combine_notification_message(string message, int timeLeft)
 		{
 			int[] convertedTimeLeft = calculate_time_from_seconds (timeLeft);
 			string timeLeftString = "";
-			
+
 			if(convertedTimeLeft[0] != 0)
-				timeLeftString += convertedTimeLeft[0] + " " + 
+				timeLeftString += convertedTimeLeft[0] + " " +
 					((convertedTimeLeft[0] == 1) ? PurpleI18n.Get ("day") : PurpleI18n.Get ("days")) + " ";
 			if(convertedTimeLeft[1] != 0)
-				timeLeftString += convertedTimeLeft[1] + " " + 
+				timeLeftString += convertedTimeLeft[1] + " " +
 					((convertedTimeLeft[1] == 1) ? PurpleI18n.Get ("hour") : PurpleI18n.Get ("hours")) + " ";
 			if(convertedTimeLeft[2] != 0)
-				timeLeftString += convertedTimeLeft[2] + " " + 
+				timeLeftString += convertedTimeLeft[2] + " " +
 					((convertedTimeLeft[2] == 1) ? PurpleI18n.Get ("minute") : PurpleI18n.Get ("minutes")) + " ";
 			if(convertedTimeLeft[3] != 0)
-				timeLeftString += convertedTimeLeft[3] + " " + 
+				timeLeftString += convertedTimeLeft[3] + " " +
 					((convertedTimeLeft[3] == 1) ? PurpleI18n.Get ("second") : PurpleI18n.Get ("seconds")) + " ";
-			
+
 			return message.Replace(notificationPlaceholder, timeLeftString.Trim());
 		}
-		
+
 		private int[] calculate_time_from_seconds(int timeLeft)
 		{
 			TimeSpan time = TimeSpan.FromSeconds( timeLeft );
 			return new int[] { time.Days, time.Hours, time.Minutes, time.Seconds };
 		}
-		
+
 		private _PurpleMessages.Server.Message create_broadcast_message(string data)
 		{
 			_PurpleMessages.Server.Message PSM = new _PurpleMessages.Server.Message ();
 			PSM.message = data;
 			return PSM;
 		}
-		
+
 		private void initialize_database_connection(ServerConfig config)
 		{
-			PurpleDatabase.PurpleDatabase.Setup(config.DatabaseHost, config.DatabaseName, 
+			PurpleDatabase.PurpleDatabase.Setup(config.DatabaseHost, config.DatabaseName,
 			                                    config.DatabaseUser, config.DatabasePassword, config.DatabasePort);
 		}
 	}
--- PurpleScripts/Network/Server/PurpleNetworkServerSanityTester.cs
+++ PurpleScripts/Network/Server/PurpleNetworkServerSanityTester.cs
@@ -6,14 +6,18 @@ using System.Net;
 using UnityEngine;
 using PurpleNetwork.Server;
 using Entities.PurpleNetwork.Server;
+using Entities.Database;
+using PurpleDatabase;
+
 
 // TODO: Class for monitoring - split it out later on
 
+
 namespace PurpleNetwork
 {
-	public class PurpleNetworkTester : MonoBehaviour
+	public class PurpleNetworkServerSanityTester : MonoBehaviour
 	{
-		private static PurpleNetworkTester instance;
+		private static PurpleNetworkServerSanityTester instance;
 
 		private static string formerIP;
 		private static int formerPort;
@@ -27,6 +31,12 @@ namespace PurpleNetwork
 
 		private static ServerReference currentServerReference;
 
+		private static PurpleCountdown sanityCountdown;
+		private static PurpleCountdown sanityCountdownDone;
+
+		private static bool server_sanity_database;
+		private static bool server_sanity_network_ip;
+		private static bool server_sanity_network_reachable;
 
 		private class pingData
 		{
@@ -47,7 +57,7 @@ namespace PurpleNetwork
 		}
 
 		// START UP /////////////////////////
-		protected PurpleNetworkTester ()
+		protected PurpleNetworkServerSanityTester ()
 		{
 			formerIP = Network.connectionTesterIP;
 			formerPort = Network.connectionTesterPort;
@@ -61,14 +71,14 @@ namespace PurpleNetwork
 
 
 		// SINGLETON /////////////////////////
-		private static PurpleNetworkTester Instance
+		private static PurpleNetworkServerSanityTester Instance
 		{
 			get
 			{
 				if (instance == null)
 				{
 					GameObject gameObject 	= new GameObject ("PurpleNetworkTester");
-					instance     			= gameObject.AddComponent<PurpleNetworkTester> ();
+					instance     			= gameObject.AddComponent<PurpleNetworkServerSanityTester> ();
 				}
 				return instance;
 			}
@@ -76,12 +86,19 @@ namespace PurpleNetwork
 
 
 		// PUBLIC FUNCTIONS /////////////////////////
+		// SERVER SANITY CHECK /////////////////////////
+		public static void ServerSanityCheck()
+		{
+			Instance.server_sanity_check ();
+		}
+
+
 		// SIMPLE PING /////////////////////////
 		public static int Ping(IPAddress ipAddress)
 		{
 			return Instance.ping(ipAddress);
 		}
-		
+
 		public static int Ping(string host)
 		{
 			return Instance.ping(host);
@@ -89,6 +106,11 @@ namespace PurpleNetwork
 
 
 		// ADVANCED FUNCTIONS /////////////////////////
+		public static string Run()
+		{
+			return Instance.run_test (30).ToString();
+		}
+
 		public static string Run(string ipAddress, int port)
 		{
 			return Instance.run_test (ipAddress, port, 30).ToString();
@@ -120,6 +142,11 @@ namespace PurpleNetwork
 			}
 		}
 
+		public static bool TestDatabase(string ip, string externalIp)
+		{
+			return Instance.test_current_database_connection (ip, externalIp);
+		}
+
 
 
 		// PRIVATE FUNCTIONS /////////////////////////
@@ -131,7 +158,7 @@ namespace PurpleNetwork
 			}
 			return ping ();
 		}
-		
+
 		private int ping(string host)
 		{
 			if(pingObject.host != host)
@@ -141,15 +168,15 @@ namespace PurpleNetwork
 			}
 			return ping ();
 		}
-		
+
 		private int ping()
 		{
 			if(pingObject.ping != null && pingObject.ping.isDone)
 				pingObject.lastPing = pingObject.ping.time;
-			
+
 			if(pingObject.ping == null || pingObject.ping.isDone)
 				pingObject.ping = new Ping (pingObject.IP.ToString());
-			
+
 			return pingObject.lastPing;
 		}
 
@@ -170,13 +197,27 @@ namespace PurpleNetwork
 			{
 				testDone = false;
 				init_connection (ipAddress, port);
-				countdown = PurpleCountdown.NewInstance ();
+				countdown = PurpleCountdown.NewInstance ("ConnectionTesterStatus");
 				countdown.CountdownRunEvent += test_connection;
 				countdown.CountdownDoneEvent += reset_connection;
 				countdown.CountDown (timeout);
 			}
 			return testResult;
 		}
+
+		private ConnectionTesterStatus run_test(int timeout)
+		{
+			if(testDone)
+			{
+				testDone = false;
+				countdown = PurpleCountdown.NewInstance ("ConnectionTesterStatus");
+				countdown.CountdownRunEvent += test_connection;
+				countdown.CountdownDoneEvent += reset_connection;
+				countdown.CountDown (timeout);
+			}
+			return testResult;
+		}
+
 
 		private bool get_test_state()
 		{
@@ -187,13 +228,13 @@ namespace PurpleNetwork
 		{
 			return testResult;
 		}
-		
+
 		// PRIVATE HELPER /////////////////////////
 		private void init_connection(string ipAddress, int port)
 		{
 			formerIP = Network.connectionTesterIP;
 			Network.connectionTesterIP = ipAddress;
-			
+
 			formerPort = Network.connectionTesterPort;
 			Network.connectionTesterPort = port;
 		}
@@ -252,7 +293,7 @@ namespace PurpleNetwork
 				sr.ReferencePingNote = newSR.ReferencePingNote;
 				sr.ServerState = newSR.ServerState;
 				sr.ReferenceLastSeen = newSR.ReferenceLastSeen;
-				
+
 				if(returnValue)
 					returnValue = pingReturn;
 			}
@@ -264,31 +305,144 @@ namespace PurpleNetwork
 		{
 			return Ping (reference.ServerHost);
 		}
-		
+
 		public static ServerReference Test(ServerReference reference)
 		{
 			string pingMessage = String.Empty;
 			ServerReference newRefernece = reference;
 			bool pingReturn = Ping(reference.ServerHost, out pingMessage);
-			
+
 			newRefernece.ReferencePingNote = pingMessage;
 			if(pingReturn)
 			{
 				newRefernece.ServerState = ServerStates.Online;
 				newRefernece.ReferenceLastSeen = DateTime.Now;
-			} 
-			else 
+			}
+			else
 			{
 				newRefernece.ServerState = ServerStates.Offline;
 			}
 			return newRefernece;
 		}
-		
+
 		public bool Test(ServerReference reference, out string pingMessage)
 		{
 			return Ping (reference.ServerHost, out pingMessage);
 		}
 		*/
 
+
+		private void server_sanity_check()
+		{
+			server_sanity_database = false;
+			server_sanity_network_ip = false;
+			server_sanity_network_reachable = false;
+
+			int testTime = 30;
+
+			Debug.Log ("ServerSanityCheck: Start...");
+			Debug.Log ("ServerSanityCheck: Initialize calls...");
+			run_test (testTime-2);
+			string ipAddress = Network.player.ipAddress;
+			string externalIP = Network.player.externalIP;
+
+			Debug.Log ("ServerSanityCheck: Start Database check...");
+			if(test_current_database_connection (ipAddress, externalIP))
+			{
+				Debug.Log ("ServerSanityCheck: Database OK");
+				server_sanity_database = true;
+			}
+			else
+			{
+				Debug.LogError("ServerSanityCheck: Database ERROR");
+			}
+
+			Debug.Log ("ServerSanityCheck: Start network check...");
+			Debug.Log ("ServerSanityCheck: Local IP Address " + ipAddress);
+			Debug.Log ("ServerSanityCheck: External IP Address " + externalIP);
+			if(!string.IsNullOrEmpty(externalIP)
+			   && !externalIP.StartsWith("0.")
+			   && !externalIP.StartsWith("127.")
+			   && !externalIP.StartsWith("192."))
+			{
+				server_sanity_network_ip = true;
+			}
+
+			sanityCountdown = PurpleCountdown.NewInstance ("SanityCheck");
+			sanityCountdown.TriggerEvent += server_sanity_check_periodical;
+			sanityCountdown.Trigger (5, testTime/10);
+
+			sanityCountdownDone = PurpleCountdown.NewInstance ("SanityCheckKill");
+			sanityCountdownDone.CountdownDoneEvent += server_sanity_check_done;
+			sanityCountdownDone.CountDown (testTime);
+		}
+
+		private void server_sanity_check_periodical()
+		{
+			if(testDone)
+			{
+				Debug.Log ("ServerSanityCheck: Network Testresult " + testResult.ToString());
+				if(testResult != ConnectionTesterStatus.Error &&
+				   testResult != ConnectionTesterStatus.Undetermined)
+				{
+					server_sanity_network_reachable = true;
+				}
+				server_sanity_check_done();
+			}
+			else
+			{
+				Debug.Log ("ServerSanityCheck: Network Testresult still running...");
+			}
+		}
+
+		private void server_sanity_check_done()
+		{
+			sanityCountdown.TriggerEvent -= server_sanity_check_periodical;
+			sanityCountdown.CancelTrigger();
+			sanityCountdown.DestroyInstance();
+
+			sanityCountdownDone.CountdownDoneEvent -= server_sanity_check_done;
+			sanityCountdownDone.CancelCountDown();
+			sanityCountdownDone.DestroyInstance();
+
+			if(server_sanity_database && server_sanity_network_ip && server_sanity_network_reachable)
+			{
+				Debug.Log("ServerSanityCheck: Success!");
+			}
+			else
+			{
+				Debug.LogError("ServerSanityCheck: ERROR!");
+				if(!server_sanity_database)
+					Debug.LogError("ServerSanityCheck: Database unreachable!");
+
+				if(!server_sanity_network_ip)
+					Debug.LogError("ServerSanityCheck: No external IP!");
+
+				if(!server_sanity_network_reachable)
+					Debug.LogError("ServerSanityCheck: Network Error!");
+
+
+				Debug.LogError("ServerSanityCheck: Shut down server...");
+				if(PurpleServer.CurrentConfig.ServerSanityTest)
+					PurpleServer.StopServer(5);
+			}
+		}
+
+		private bool test_current_database_connection(string ip, string externalIp)
+		{
+			Entities.Database.PurpleServerLog psl = new Entities.Database.PurpleServerLog ();
+			psl.name = PurpleServer.CurrentConfig.ServerName;
+			psl.port = PurpleServer.CurrentConfig.ServerPort;
+			psl.max_player = PurpleServer.CurrentConfig.ServerMaxClients;
+			psl.host = PurpleServer.CurrentConfig.ServerHost;
+			psl.comment = String.Empty;
+			psl.type = PurpleServer.CurrentConfig.ServerType.ToString();
+			psl.local_ip = ip;
+			psl.global_ip = externalIp;
+
+			int insertResult = psl.ToSQLInsert ().Execute ();
+
+			return (insertResult==1) ? true : false;
+		}
 	}
 }
