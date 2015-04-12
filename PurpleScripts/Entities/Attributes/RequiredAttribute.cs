f601a4edca2ca34fd7365b08d41973d8a0d2ba36
Max <1@porzelt.net>
2015-04-13 00:59:04 +0200

    add validation call

--- PurpleScripts/Entities/Attributes/RequiredAttribute.cs
+++ PurpleScripts/Entities/Attributes/RequiredAttribute.cs
@@ -1,11 +1,12 @@
 using System;
+using System.Reflection;
 
 namespace PurpleAttributes
 {
 	public class RequiredAttribute : Attribute
 	{
-		public string ErrorMessage;
-		public bool Required;
+		private string ErrorMessage;
+		private bool Required;
 
 		public RequiredAttribute()
 		{
@@ -23,6 +24,54 @@ namespace PurpleAttributes
 		{
 			this.ErrorMessage = ErrorMessage;
 			this.Required = Required;
+		}
+
+		public bool validate<T>(T data, PropertyInfo singleProperty, out string error)
+		{
+			error = string.Empty;
+			if(this.Required == false)
+				return true;
+
+			bool returnValue = true;
+
+			try {
+				switch (Type.GetTypeCode(singleProperty.PropertyType))
+				{
+				case TypeCode.DateTime:
+					DateTime dateValue = (DateTime)singleProperty.GetValue(data, null);
+					DateTime dtNull = Convert.ToDateTime("0001-01-01 00:00:00");
+					if(dateValue == null || dateValue.Equals(dtNull) || dateValue.Equals(DateTime.MinValue))
+						returnValue =  false;
+					break;
+
+				case TypeCode.Boolean:
+					break;
+
+				case TypeCode.Int32:
+					int intValue = Convert.ToInt32(singleProperty.GetValue(data, null));
+					if (intValue == null || intValue == 0)
+						returnValue =  false;
+					break;
+
+				case TypeCode.String:
+					string stringValue = singleProperty.GetValue(data, null).ToString();
+					if(string.IsNullOrEmpty(stringValue) || stringValue.Equals("NULL"))
+						returnValue =  false;
+					break;
+
+				default:
+					object defaultValue = singleProperty.GetValue(data, null);
+					if(defaultValue == null)
+						returnValue =  false;
+					break;
+				}
+			} catch {
+				returnValue =  false;
+			}
+
+			if (!returnValue)
+				error = this.ErrorMessage + " " + singleProperty.Name;
+			return returnValue;
 		}
 	}
 }
