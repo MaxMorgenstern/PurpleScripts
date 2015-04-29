327a536135dc1f220426c57d3b33422c4d9b6cbb
Max <1@porzelt.net>
2015-04-30 00:18:23 +0200

    Update client delegates and shared

--- PurpleScripts/Network/Client/Handler/PurpleNetworkBaseHandler.cs
+++ PurpleScripts/Network/Client/Handler/PurpleNetworkBaseHandler.cs
@@ -4,8 +4,10 @@ using _PMServer = Entities.PurpleMessages.Server;
 
 namespace PurpleNetwork.Client.Handler
 {
-	public class Base
+	public class Base : Shared
 	{
+		public static event PurpleNetworkClientEvent ServerBroadcast;
+
 		public static void register_base_handler()
 		{
 			PurpleNetwork.ConnectedToPurpleServer += connected_to_server_handler;
@@ -16,6 +18,7 @@ namespace PurpleNetwork.Client.Handler
 			PurpleNetwork.AddListener("server_generate_token_result", server_generate_token_result_handler);
 			PurpleNetwork.AddListener("server_logout_result", server_logout_result_handler);
 			PurpleNetwork.AddListener("server_ping", server_ping_handler);
+			PurpleNetwork.AddListener("server_switch", server_switch_handler);						//TODO: Impver Ca	PurpleNetwork.AddListener("server_version_result", server_version_result_handler);
 
 			PurpleNetwork.DisconnectedFromPurpleServer += remove_base_handler;
 		}
@@ -29,30 +32,67 @@ namespace PurpleNetwork.Client.Handler
 			Debug.Log ("Broadcast received: " + np.ToString () + " | " + dataObject);
 			if(np.ToString() != Constants.SERVER_ID_STRING) return;
 
-			// TODO:
+			// TODO: more?
+			trigger_purple_event (ServerBroadcast, dataObject);
 		}
 
 		public static void server_authenticate_result_handler (string dataObject, NetworkPlayer np)
 		{
 			_PMClient.Authentication authObject = PurpleSerializer.StringToObjectConverter<_PMClient.Authentication> (dataObject);
-			//TODO
+			if (authObject.validate)
+			{
+				PurpleClient.CurrentConfig.PlayerAuthenticated = authObject.ClientAuthenticated;
+				if (!authObject.ClientAuthenticated)
+				{
+					// TODO: too many clients connected
+					return;
+				}
+				PurpleClient.CurrentConfig.ClientToken = authObject.ClientToken;
+				PurpleClient.CurrentConfig.ClientTokenCreated = authObject.timestamp;
+
+			} 
+			else 
+			{
+				// TODO: incorect data
+			}
 		}
 
 		public static void server_generate_token_result_handler (string dataObject, NetworkPlayer np)
 		{
 			_PMClient.Authentication authObject = PurpleSerializer.StringToObjectConverter<_PMClient.Authentication> (dataObject);
-			//TODO
+			if(!string.IsNullOrEmpty (authObject.ClientToken))
+			{
+				PurpleClient.CurrentConfig.ClientToken = authObject.ClientToken;
+				PurpleClient.CurrentConfig.ClientTokenCreated = authObject.timestamp;
+			}
+			// TODO: trigger?
 		}
 
 		public static void server_logout_result_handler (string dataObject, NetworkPlayer np)
 		{
 			_PMClient.Authentication authObject = PurpleSerializer.StringToObjectConverter<_PMClient.Authentication> (dataObject);
-			//TODO
+			if(!authObject.ClientAuthenticated)
+			{
+
+				// TODO: logged out - okay
+			}
 		}
 
 		public static void server_ping_handler (string dataObject, NetworkPlayer np)
 		{
 			_PMServer.Ping pingObject = PurpleSerializer.StringToObjectConverter<_PMServer.Ping> (dataObject);
+			//TODO
+		}
+
+		public static void server_switch_handler (string dataObject, NetworkPlayer np)
+		{
+			_PMServer.SwitchMessage switchObject = PurpleSerializer.StringToObjectConverter<_PMServer.SwitchMessage> (dataObject);
+			//TODO
+		}
+
+		public static void server_version_result_handler (string dataObject, NetworkPlayer np)
+		{
+			_PMServer.Version switchObject = PurpleSerializer.StringToObjectConverter<_PMServer.Version> (dataObject);
 			//TODO
 		}
 
---
+++ PurpleScripts/Network/Client/Handler/PurpleNetworkShared.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Entities.PurpleNetwork;
+
+namespace PurpleNetwork.Client.Handler
+{
+	public class Shared
+	{
+		protected static void trigger_purple_event(PurpleNetworkClientEvent eve)
+		{
+			trigger_purple_event (eve, null);
+		}
+
+		protected static void trigger_purple_event(PurpleNetworkClientEvent eve, object passed_object)
+		{
+			if(eve != null)
+				eve(passed_object);
+		}
+	}
+}
+
